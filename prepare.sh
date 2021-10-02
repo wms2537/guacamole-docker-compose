f2d6438322f93013593946cec4985599fa770654
wms2537 <swmeng@yes.my>
2021-10-02 21:34:02 +0800

    fix bugs

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,6 @@
 #
 echo "Preparing folder init and creating ./init/initdb.sql"
 mkdir ./init >/dev/null 2>&1
-mkdir -p ./nginx/ssl >/dev/null 2>&1
 chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./init/initdb.sql
 echo "done"
